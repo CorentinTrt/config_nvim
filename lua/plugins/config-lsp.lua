0480013a20afb571fc6ea7adc6f0f72d117edcac
CorentinTrt <corentintruffaut@gmail.com>
2025-12-18 20:23:06 -0500

    feat(lsp): add bash_ls

--- lua/plugins/config-lsp.lua
+++ lua/plugins/config-lsp.lua
@@ -19,18 +19,13 @@ return {
     },
     opts = {
       ensure_installed = {
+        "bashls",
         "lua_ls",
         "ts_ls",
       },
     },
     config = function(_, opts)
       require("mason-lspconfig").setup(opts)
-      -- local handlers = {
-      --   function(server_name)
-      --     require("lspconfig")[server_nasme].setup({})
-      --   end,
-      -- }
-      -- require("mason-lspconfig").setup_handlers(handlers)
     end,
   },
 
@@ -41,12 +36,10 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      -- "hrsh7th/cmp-nvim-lsp",
     },
     opts = {},
     config = function(_, opts)
-      -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- vim.lsp.config("*", { capabilities = capabilities })
+      vim.lsp.enable("bashls")
       vim.lsp.enable("lua_ls")
     end,
   },
