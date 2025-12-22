5a4ed3839d72897c1ad20df5ed15fa9c612085c8
CorentinTrt <corentintruffaut@gmail.com>
2025-12-22 18:16:01 -0500

    fix(telescope): include opts in function for first install

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,30 +7,32 @@ return
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
   },
-  opts = {
-    defaults = {
-      file_ignore_patterns = {
-        "node_modules/",
-        ".git/",
-        "-lock.json",
+  opts = function()
+    return {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules/",
+          ".git/",
+          "-lock.json",
+        },
       },
-    },
-    pickers = {
-      find_files = {
-        hidden = true,
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
       },
-    },
-    extensions = {
-      file_browser = {
-        grouped = true,
-        hijack_netrw = true,
-        hidden = true,
+      extensions = {
+        file_browser = {
+          grouped = true,
+          hijack_netrw = true,
+          hidden = true,
+        },
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
+        },
       },
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown({}),
-      },
-    },
-  },
+    }
+  end,
   config = function(_, opts)
     require("telescope").setup(opts)
     require("telescope").load_extension("ui-select")
